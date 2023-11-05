29e62a4701651a299d7efe385698bff7111a09c9
linzetai <Linzetaibaylor521@gmail.com>
2023-11-05 21:25:12 +0800

    主题色定制

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,3 +1,4 @@
+import { Button } from "antd-mobile"
 import { Outlet } from "react-router"
 
 const Layout = () => {
@@ -5,6 +6,10 @@ const Layout = () => {
         <div>
             <Outlet></Outlet>
             <span>我是Layout</span>
+            <Button color="primary">测试全局</Button>
+            <div className="puple">
+                <Button color="primary">测试局部</Button>
+            </div>
         </div>
     )
 }
