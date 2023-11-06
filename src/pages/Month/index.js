6d331b58e323a41197d77cb8056fcc064aff79f2
linzetai <Linzetaibaylor521@gmail.com>
2023-11-06 21:31:45 +0800

    统计模块 当前月份

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -2,11 +2,17 @@ import { NavBar, DatePicker } from 'antd-mobile'
 import { useState } from 'react'
 import classNames from 'classnames'
 import './index.scss'
+import dayjs from 'dayjs'
 
 const Month = () => {
     const [dateVisible, setDateVisible] = useState(false)
-    const handleDateConfirm = () => {
+    const [currentDate, setCurrentDate] = useState(() => {
+        return dayjs(new Date()).format('YYYY-MM')
+    })
+    const handleDateConfirm = (date) => {
         setDateVisible(false)
+        const formatDate = dayjs(date).format('YYYY-MM')
+        setCurrentDate(formatDate)
     }
     return (
         <div className="monthlyBill">
@@ -18,7 +24,7 @@ const Month = () => {
                     {/* 时间切换区域 */}
                     <div className="date">
                         <span className="text">
-                            2023 | 3月账单
+                            {currentDate} 月账单
                         </span>
                         <span className={classNames('arrow', dateVisible && 'expand')} onClick={() => setDateVisible(true)}></span>
                     </div>
